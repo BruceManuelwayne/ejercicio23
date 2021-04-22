c4b354eaa2fa5163e5e7113464ac4305381572a6
Enrique Green <47195508+BruceManuelwayne@users.noreply.github.com>
2021-04-22 08:23:33 -0300

    validacion fecha valida

---
+++ ejercicio23/ejercicio23/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateString;
+            bool fechaValida=true;
+            DateTime parsedDate; 
+
+            Console.WriteLine("Ingrese una fecha: ");
+            do
+            {
+                dateString = Console.ReadLine();
+                if(DateTime.TryParse(dateString, out parsedDate))
+                {
+                    Console.WriteLine(parsedDate);
+                    break; 
+                }
+                else
+                {
+                    Console.WriteLine("formato invalido, ingrese otra fecha: ");
+                    fechaValida = false;
+                   
+                }
+
+
+            } while (!fechaValida);
+            Console.ReadKey(); 
+
+        }
+    }
+}
